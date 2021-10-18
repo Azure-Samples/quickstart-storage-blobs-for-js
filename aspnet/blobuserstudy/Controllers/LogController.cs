de36c0381c293fe8644fc1110bfb474732b8fcb8
Kyle P <kylepa@microsoft.com>
2021-10-18 10:56:43 -0700

    Create asp.net template (#2)
    * update gitignore to include asp.net
    
    * Add aspnet template

---
+++ aspnet/blobuserstudy/Controllers/LogController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blobuserstudy.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger<LogController> _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+
+            // TODO: Complete this section to retrieve a list of blobs in a container
+
+            var res = "A sample response";
+            return res;
+        }
+
+        [HttpPost]
+        public async Task<string> Post()
+        {
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                var body = await reader.ReadToEndAsync();
+
+                // TODO: Complete this section to upload the body ("log data") to a Blob
+                return body;
+            }
+        }
+    }
+}
